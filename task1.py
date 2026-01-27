5986643e267b33e08e97246bb13cf87c4ca929f9
markjconnor <123944608+markjconnor@users.noreply.github.com>
2026-01-27 16:28:34 +0000

    Implement pandas table for task1

--- task1.py
+++ task1.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+
 DATASET="passage-collection.txt"
 
-def d1_1(dataset):
+def get_vocabulary(dataset):
     with open(dataset) as f:
         text = f.read()
         words = text.split()
 
     vocabulary = set(words)
-    print("Report the size of the identified index of terms (vocabulary):", len(vocabulary))
+    #print("Report the size of the identified index of terms (vocabulary):", len(vocabulary))
 
 """
 implement a function (or a block of source code) that 
@@ -30,28 +32,38 @@ def count_term_frequency(dataset="passage-collection.txt"):
 
   return term_frequency
 
-if __name__ == "__main__":
-    d1_1(DATASET)
+def calculate_zipf_distribution(rank, vocabulary_size):
+   return 1 / rank / sum(1 / r for r in range(1, vocabulary_size + 1))
 
+if __name__ == "__main__":
+    get_vocabulary(DATASET)
     tf = count_term_frequency(DATASET)
 
-
     ranked_terms = sorted(tf.items(), key=lambda x: x[1], reverse=True)
+    rows = []
 
-    ranks = []
-    occurrence_probabilities = []
+    LIMIT = 50
+    for rank, (term, freq) in enumerate(ranked_terms[:LIMIT], start=1):
+        normalised_occurrence = (freq / len(tf)) / 100
 
-    for rank, (term, freq) in enumerate(ranked_terms, start=1):
-        occurrence_probability = freq / len(tf)
-        ranks.append(rank)
-        occurrence_probabilities.append(occurrence_probability)
+        rows.append({
+           "Word": term, 
+            "Rank": rank,
+           "Normalised Frequency": normalised_occurrence,
+           "rank*frequency": rank * normalised_occurrence,
+           "Zipf's Law Distribution": calculate_zipf_distribution(rank, len(tf))
+        })
 
+    df = pd.DataFrame(rows)
+    print(df)
+"""
+    RANK_LIMIT = 50
     plt.figure()
-    plt.plot(ranks, occurrence_probabilities)
+    plt.plot(ranks[0:RANK_LIMIT], occurrence_probabilities[0:RANK_LIMIT])
     plt.xlabel("Rank")
     plt.ylabel("Occurrence Probability")
     plt.title("Rank vs Occurrence Probability")
     plt.show()
-
+"""
 
        
