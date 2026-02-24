bbd858ac12e5cae1f579f4d6f938f8dd6e841b90
markjconnor <123944608+markjconnor@users.noreply.github.com>
2026-02-24 14:10:28 +0000

    Remove stop words in task1 and fix performance

--- task1.py
+++ task1.py
@@ -1,45 +1,65 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 
 DATASET="passage-collection.txt"
 
-def get_vocabulary(dataset):
+def get_vocabulary(dataset, stop_words):
+    
     with open(dataset) as f:
         text = f.read()
+        # TODO: In report, TEXT PRE-PROCESSING CHOICE: Chose to ignore case sensitivity
+        text = text.lower()
         words = text.split()
-
+        if stop_words:
+            words = [word for word in words if word not in stop_words]
     vocabulary = set(words)
-    #print("Report the size of the identified index of terms (vocabulary):", len(vocabulary))
 
-"""
-implement a function (or a block of source code) that 
-counts the number of occurrences of terms in the provided data set, 
-plot (Figure 1) their probability of occurrence (normalised frequency) 
-against their frequency ranking, and qualitatively justify that 
-these terms follow Zipf’s law."""
-def count_term_frequency(dataset="passage-collection.txt"):
+    print("Size of the vocabulary = ", len(vocabulary)) 
+    return vocabulary
+
+def count_term_frequency(dataset, stop_words):
   with open(dataset) as f:
     text = f.read()
     words = text.split()
+    if stop_words:
+        words = [word for word in words if word not in stop_words]
 
   term_frequency = {}
   for word in words:
-    word = word.lower()
     term_frequency[word] = term_frequency.get(word, 0) + 1
-
 
   return term_frequency
 
-def calculate_zipf_distribution(rank, vocabulary_size):
-   return 1 / rank / sum(1 / r for r in range(1, vocabulary_size + 1))
+def plot_figure_1(df):
+   # Figure 1: plot normalised frequency against frequency ranking
+    plt.figure(figsize=(10, 6))
+    plt.plot(df["Rank"], df["Normalised Frequency"], marker='o')
+    plt.xlabel('Rank')
+    plt.ylabel('Normalised Frequency')
+    plt.title('Term Frequency vs Rank')
+    plt.grid(True)
+    plt.savefig("figure_1.png")
 
-if __name__ == "__main__":
-    get_vocabulary(DATASET)
-    tf = count_term_frequency(DATASET)
+def plot_figure_2(df):
+   # Figure 2: create a log log plot of Rank*Frequency vs Zipf's Law Distribution
+    plt.figure(figsize=(10, 6))
+    plt.loglog(df["Rank"], df["Normalised Frequency"], marker='o', label='Observed Data')
+    plt.loglog(df["Rank"], df["Zipf's Law Distribution"], marker='x', label="Zipf's Law", linestyle='--')
+    plt.xlabel('Rank')
+    plt.ylabel('Normalised Frequency')
+    plt.title("Zipf's Law: Term Frequency vs Rank")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("figure_2.png")
 
+def construct_df(tf):
     ranked_terms = sorted(tf.items(), key=lambda x: x[1], reverse=True)
     rows = []
 
+    denominator = sum(1 / i for i in range(1, len(tf) + 1))
 
     for rank, (term, freq) in enumerate(ranked_terms, start=1):
         normalised_occurrence = (freq / len(tf)) / 100
@@ -49,19 +69,39 @@ if __name__ == "__main__":
             "Rank": rank,
            "Normalised Frequency": normalised_occurrence,
            "rank*frequency": rank * normalised_occurrence,
-           "Zipf's Law Distribution": calculate_zipf_distribution(rank, len(tf))
+           "Zipf's Law Distribution": (1 / rank / denominator)
         })
 
-    df = pd.DataFrame(rows)
-    print(df.head(10))
+    return pd.DataFrame(rows)
 
-    # Figure 2: create a log log plot of Rank*Frequency vs Zipf's Law Distribution
+"""
+How will the difference between the two distributions be affected if we also remove stop
+words? Justify your answer by depicting (Figure 3) and quantifying this difference
+"""
+def plot_figure_3(df, df_stop_words_removed):
     plt.figure(figsize=(10, 6))
     plt.loglog(df["Rank"], df["Normalised Frequency"], marker='o', label='Observed Data')
-    plt.loglog(df["Rank"], df["Zipf's Law Distribution"], marker='x', label="Zipf's Law", linestyle='--')
+    plt.loglog(df_stop_words_removed["Rank"], df_stop_words_removed["Normalised Frequency"], marker='o', label='Observed Data (Stop Words Removed)')
     plt.xlabel('Rank')
     plt.ylabel('Normalised Frequency')
-    plt.title("Zipf's Law: Term Frequency vs Rank")
+    plt.title("Zipf's Law: Term Frequency vs Rank (Stop Words Removed)")
     plt.legend()
     plt.grid(True)
-    plt.show()
+    plt.savefig("figure_3.png")
+
+if __name__ == "__main__":
+    vocabulary = get_vocabulary(DATASET, None)
+    tf = count_term_frequency(DATASET, None)
+    df = construct_df(tf)
+    
+
+    plot_figure_1(df)
+    plot_figure_2(df)
+
+    stop_words = stopwords.words('english')
+    vocabulary2 = get_vocabulary(DATASET, stop_words)
+    tf2 = count_term_frequency(DATASET, stop_words)
+    df2 = construct_df(tf2)
+    plot_figure_3(df, df2)
+
+  
